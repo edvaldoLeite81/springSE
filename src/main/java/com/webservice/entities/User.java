24bf756e7b5d9d6f76f6b6ba895ad6a480a30508
Edvaldo G Leite <eguilhermeleite@gmail.com>
2023-03-22 16:01:07 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/webservice/entities/User.java
+++ src/main/java/com/webservice/entities/User.java
@@ -2,6 +2,11 @@ package com.webservice.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,10 +18,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	 @EqualsAndHashCode.Include
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
 	 
 	 private String name;
