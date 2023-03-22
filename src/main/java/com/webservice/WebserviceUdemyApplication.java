3081547d3a26b868a2457614d4d7bbe92dc3cbfa
Edvaldo G Leite <eguilhermeleite@gmail.com>
2023-03-22 15:11:15 -0300

    Project created

---
+++ src/main/java/com/webservice/WebserviceUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceUdemyApplication.class, args);
+	}
+
+}
